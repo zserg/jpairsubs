b54dbe9415d0c7d3ac324092c8bdc0c27dffdf8d
Sergey Zadyabin <zsergt3@gmail.com>
2020-10-24 17:27:36 +0300

    initial commit

---
+++ src/main/java/com/zserg/jpairsubs/JpairsubsApplication.java
@@ -0,0 +1,13 @@
+package com.zserg.jpairsubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpairsubsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpairsubsApplication.class, args);
+	}
+
+}
